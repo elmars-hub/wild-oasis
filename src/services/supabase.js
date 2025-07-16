4d03c5c997e91b4cc70b95428d8c3fb85efbb033
elmars-hub <martinsifeanyi247@gmail.com>
2025-07-16 02:40:58 +0100

    feat: react-query

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hzbyofuwfzekangmykls.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6YnlvZnV3Znpla2FuZ215a2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0ODA5NjIsImV4cCI6MjA2ODA1Njk2Mn0.Mgjz11CFe56cEpzHBSNqG985CghN2RxWglGOtrkXu_k";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
