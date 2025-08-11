c90a6107820a1684b6637f0163c46ea7478ecfef
elmars-hub <martinsifeanyi247@gmail.com>
2025-08-11 08:04:52 +0100

    feat: logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,8 +11,8 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
       toast.success("Login Successful");
     },
 
