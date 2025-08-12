e843583a8031a4dc38508f25595b0ff32939e098
elmars-hub <martinsifeanyi247@gmail.com>
2025-08-12 07:39:08 +0100

    feat: sales chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -35,7 +36,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Todays Activity</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
       <div>Statistics</div>
       <div>Statistics</div>
     </StyledDashboardLayout>
